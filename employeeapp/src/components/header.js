a8174609d3997d2252df5307c7592087cc4fb58b
email <jjazminclrk@gmail.com>
2021-02-24 20:06:47 -0500

    adding all remaining files for submission

--- employeeapp/src/components/header.js
+++ employeeapp/src/components/header.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Header extends React.Component {
